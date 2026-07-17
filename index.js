ef0e243563e5b500c4a0098988b14965c7f42b4b
salman936-wq <salmansardar936@gmail.com>
2026-07-17 18:13:08 +0600

    mongodb setup done go namaz

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const ImageKit = require("imagekit");
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const PORT = process.env.PORT || 5500;
@@ -17,6 +18,12 @@ const imagekit = new ImageKit({
   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
 
+
+// Basic Route
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
 app.get("/imagekit/auth", (req, res) => {
   const authenticationParameters =
     imagekit.getAuthenticationParameters();
@@ -25,10 +32,43 @@ app.get("/imagekit/auth", (req, res) => {
 });
 
 
-// Basic Route
-app.get('/', (req, res) => {
-  res.send('Server is running!');
+
+
+
+const uri = process.env.MONGO_DB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+
+    const db = client.db("BlodLink");
+    const usersCollection = db.collection("user");
+
+
+    app.get('/user', async (req, res)=> {
+      const usersData = await usersCollection.find().toArray();
+      res.send(usersData)
+    })
+
+
+  } finally {
+    // Keep the connection open while the server runs.
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
